0acc40488bc48776ac1a2bde1f3c07b8c22b1920
Garethjr <villarroelfranco02@gmail.com>
2024-04-26 12:52:34 -0300

    Subida de archrivos

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,6 @@
+export default function page(){ 
+return (
+
+<div><h1>hola mundo</h1><h3>ZARPADO LOJI</h3></div>
+)
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){ 
+    return (
+    
+    <div><h1>hola mundo</h1><h3>ZARPADO LOJI</h3></div>
+    )
+    }
+    
